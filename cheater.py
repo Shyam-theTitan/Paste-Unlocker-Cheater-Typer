7deed56e5cf3dcb732e86f453979393d3d37312b
Shyam Shankar <119860186+Shyam-theTitan@users.noreply.github.com>
2025-10-23 20:25:53 +0530

    Refactor cheater.py for better clipboard handling
    Updated the script to improve clipboard functionality and output formatting.

--- cheater.py
+++ cheater.py
@@ -2,9 +2,9 @@ import pyautogui
 import pyperclip
 import keyboard
 import time
-print("Cheater Typer 2.0 -- by Shyam Shankar")
+print("PAG Typer -- By S.S.Shyamshankar")
 print("Press '~' to type clipboard contents... (Press ESC to quit)")
-print("Press '$' to add clipboard contents together...")
+#print("Press '$' to add clipboard contents together...") Still testing
 print("Link to AI website: ")
 print("www.chatgpt.com")
 print("gemini.google.com")
@@ -14,24 +14,26 @@ con = 0
 while True:
     if keyboard.is_pressed('~'):
         time.sleep(0.2)
-        if con == 0:
-            text = pyperclip.paste()
-        else:
-            text = text
+        text = pyperclip.paste()
+        lines = []
+        for line in text.splitlines():
+            lines.append(line)
         pyautogui.press('backspace')
         print("Typing copied text...")
-        pyautogui.write(text, interval=0.02)
+        #pyautogui.write(text, interval=0.02)
+        for l in lines:
+            l = l.replace('\t','')
+            pyautogui.write(l)
+            pyautogui.press('enter')    
         pyautogui.keyDown('shift')
-        for i in range(12):
+        for i in range(20):
             pyautogui.press('down')
         pyautogui.keyUp('shift')
         pyautogui.press('backspace')
-    if keyboard.is_pressed('$'):
-        con = 1
-        text += pyperclip.paste()
-        text += "\n"
-
-
+    # if keyboard.is_pressed('$'):
+    #     con = 1
+    #     text += pyperclip.paste()
+    #     text += "\n"
     if keyboard.is_pressed('esc'):
         print("Exiting...")
         break
